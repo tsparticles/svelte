98c68a5f084c038e53504dfe5f0276c46cffd670
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-12-29 16:16:50 +0100

    build: repo init

---
+++ components/svelte/src/index.d.ts
@@ -0,0 +1,24 @@
+import type { SvelteComponentTyped } from "svelte";
+import type { ISourceOptions, Engine, Container } from "tsparticles-engine";
+
+declare module "@tsparticles/svelte" {
+  type CustomEventWrapper<T> = {
+    [K in keyof T]: CustomEvent<T[K]>;
+  };
+  type ParticlesProps = {
+    options?: ISourceOptions;
+    url?: string;
+    id?: string;
+    particlesInit: (engine: Engine) => Promise<void>;
+  };
+  type ParticlesEvents = CustomEventWrapper<{
+    particlesLoaded: {
+      particles?: Container;
+    };
+  }>;
+  export default class extends SvelteComponentTyped<
+    ParticlesProps,
+    ParticlesEvents,
+    {}
+  > {}
+}
