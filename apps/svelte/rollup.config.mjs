adcca3c29a59ca086e1b3db9da9c31c4f4a6421a
Greenfoot5 <greenfoot89@gmail.com>
2025-04-10 16:40:20 +0100

    Tweaked rollup config to remove warning
    "(!) [plugin typescript] @rollup/plugin-typescript: Typescript 'sourceMap' compiler option must be set to generate source maps."

--- apps/svelte/rollup.config.mjs
+++ apps/svelte/rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import terser from "@rollup/plugin-terser";
-import sveltePreprocess from "svelte-preprocess";
+import { sveltePreprocess } from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import css from "rollup-plugin-css-only";
 import json from "@rollup/plugin-json";
@@ -34,7 +34,7 @@ function serve() {
 export default {
     input: "src/main.ts",
     output: {
-        sourcemap: true,
+        sourcemap: !production,
         format: "iife",
         name: "app",
         file: "public/build/bundle.js",
